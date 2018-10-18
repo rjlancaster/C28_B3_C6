2a673d8e5d97ef46ceab3f7f343e54d6ffe40ed0
rjlancaster <rjlancaster@gmail.com>
2018-10-17 20:37:41 -0500

    Working on exercises.  Played around with all of them but really having issues comprehending : q quit git status

--- script.js
+++ script.js
@@ -1,106 +1,175 @@
 const students = [
   {
-      name: "Chris Miller",
-      class: "History",
-      info: "Failed last exam",
-      score: 59
+    name: "Chris Miller",
+    class: "History",
+    info: "Failed last exam",
+    score: 59
   },
   {
-      name: "Courtney Seward",
-      class: "History",
-      info: "Has completed all homework",
-      score: 91
+    name: "Courtney Seward",
+    class: "History",
+    info: "Has completed all homework",
+    score: 91
   },
   {
-      name: "Garrett Ward",
-      class: "History",
-      info: "Wonderful at helping other students",
-      score: 88
+    name: "Garrett Ward",
+    class: "History",
+    info: "Wonderful at helping other students",
+    score: 88
   },
   {
-      name: "John Dulaney",
-      class: "History",
-      info: "Has never missed a class or exam",
-      score: 92
+    name: "John Dulaney",
+    class: "History",
+    info: "Has never missed a class or exam",
+    score: 92
   },
   {
-      name: "Greg Lawrence",
-      class: "History",
-      info: "Sub-par performance all around",
-      score: 64
+    name: "Greg Lawrence",
+    class: "History",
+    info: "Sub-par performance all around",
+    score: 64
   },
   {
-      name: "Leah Duvic",
-      class: "History",
-      info: "Wonderful student",
-      score: 97
+    name: "Leah Duvic",
+    class: "History",
+    info: "Wonderful student",
+    score: 97
   },
   {
-      name: "Jesse Page",
-      class: "History",
-      info: "Smokes too much. Distracting.",
-      score: 76
+    name: "Jesse Page",
+    class: "History",
+    info: "Smokes too much. Distracting.",
+    score: 76
   },
   {
-      name: "Kevin Haggerty",
-      class: "History",
-      info: "Falls asleep in class",
-      score: 79
+    name: "Kevin Haggerty",
+    class: "History",
+    info: "Falls asleep in class",
+    score: 79
   },
   {
-      name: "Max Wolf",
-      class: "History",
-      info: "Talks too much",
-      score: 83
+    name: "Max Wolf",
+    class: "History",
+    info: "Talks too much",
+    score: 83
   },
   {
-      name: "Lissa Goforth",
-      class: "History",
-      info: "Asks pointless, unrelated questions",
-      score: 78
+    name: "Lissa Goforth",
+    class: "History",
+    info: "Asks pointless, unrelated questions",
+    score: 78
   },
   {
-      name: "Tyler Bowman",
-      class: "History",
-      info: "When was the last time he attended class?",
-      score: 48
+    name: "Tyler Bowman",
+    class: "History",
+    info: "When was the last time he attended class?",
+    score: 48
   },
   {
-      name: "Ray Medrano",
-      class: "History",
-      info: "Needs to contribute to in-class discussions",
-      score: 95
+    name: "Ray Medrano",
+    class: "History",
+    info: "Needs to contribute to in-class discussions",
+    score: 95
   }
 ]
 
-const h1 = (title, style) => {
-  return `<h1 class="${style}">${title}</h1>`
+// Build an HTML element
+// USING REST OPERATORS:
+
+const h1 = (...props) => {
+  return `<h1 class="${props[1]}">${props[0]}</h1>`
 }
 
-const section = (title, style) => {
-  return `<section class="bordered dashed ${style}">${title}</section>`
+const section = (...props) => {
+  return `<section class="bordered dashed ${props[1]}">${props[0]}</section>`
 }
 
-const aside = (title, style) => {
-  return `<aside class="${style}">${title}</aside>`
+const aside = (...props) => {
+  return `<aside class="${props[1]}">${props[0]}</aside>`
 }
 
-// Iterate the array of students, and apply the correct style to the h1 depending on the score of the student being below 60, or above it.
+// Build a component composed of many smaller elements
+// Joe's attempt:
 
-const student = students.name;
+const student = (name, clazz, info, status) => `
+<div id="student">
+  ${h1(name, status)}
+  ${section(clazz, "section--padded")}
+  ${aside(info, "pushRight")}
+</div>
+`
 
-for (student of students) {
-  let studentComponent = ""
-  if (student.score >= 60) {
-      studentComponent = document.getElementById("container").classList.add(".passing");
-  } else {
-      studentComponent = document.getElementById("container").classList.add(".passing");
-  }
+// my attempt:
+
+// let studentList = (ary) => {
+//   for (let i = 0; i < ary.length; i++) {
+//     if (ary[i].score >= 60) {
+//       document.querySelector("#container").innerHTML += `
+//       ${h1(ary[i].name, "xx-large passing")}
+//       `
+//     } else {
+//       document.querySelector("#container").innerHTML += `
+//       ${h1(ary[i].name, "xx-large failing")}
+//       `
+//     }
+//   }
+// }
+
+// studentList(students);
+
+
+// students.forEach((currentStudent) => {
+//   let studentComponent = ""
+//   if (currentStudent.score > 60) {
+//     studentComponent = student(currentStudent.name, currentStudent.class, currentStudent.info, "passing")
+//   } else {
+//     studentComponent = student(currentStudent.name, currentStudent.class, currentStudent.info, "failing")
+//   }
+
+//   document.querySelector("#container").innerHTML += `
+//   ${studentComponent}
+//   `
+// })
+
+
+
+// Creating Elements
+const article1 = document.querySelector("#messages")
+
+let sec = document.createElement("section");
+sec.textContent = "hello world"
+sec.classList = "message";
+
+article1.appendChild(sec);
+
+const testFrag = document.createDocumentFragment()
+
+const message = (name, frag, msg) => {
+  document.createElement('div');
+  name.textContent = msg;
+  frag.appendChild(name);
 }
 
-// If passing, the div should look like this:
-// <div class="student">
-//     <h1 class="xx-large green">Student Name</h1>
-//     <section class="bordered dashed section--padded">Student class</section>
-//     <aside class="pushRight">Additional information</aside>
-// </div> 
+message(howdy, testFrag, "howdy!!");
+
+document.querySelector("#messages").appendChild(testFrag)
+// createDocumentFragment()
+
+const fragment = document.createDocumentFragment()
+
+// Create an emperor component element
+const julius = document.createElement('div')
+julius.textContent = "Julius Caesar"
+fragment.appendChild(julius)
+
+// Create an emperor component element
+const augustus = document.createElement('div')
+augustus.textContent = "Augustus Caesar"
+fragment.appendChild(augustus)
+
+// Create an emperor component element
+const aurelius = document.createElement('div')
+aurelius.textContent = "Marcus Aurelius"
+fragment.appendChild(aurelius)
+
+document.querySelector("#emperorList").appendChild(fragment)
